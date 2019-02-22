71eee4065ddabc6afea3f34bb0cdd672bc073132
mattshardman <mattshardman@gmail.com>
2019-02-22 08:36:35 +0000

    pass props into tab component and check if tab is active

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -57,7 +57,9 @@ export default class Content extends Component {
           selectedTab={this.state.selected} 
           selectTabHandler={this.changeSelected} 
         />
-        <Cards cards={this.filterCards()} />
+        <Cards 
+          cards={this.filterCards()} 
+        />
       </div>
     );
   }
